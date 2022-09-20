058bfba0f578705f8f358d2f0c35acc15534e5bd
Kimjinwon <59737252+hdev1004@users.noreply.github.com>
2022-09-20 14:31:04 +0900

    commit

--- app/src/main/java/com/cookandroid/firebaselogin/MapActivity.java
+++ app/src/main/java/com/cookandroid/firebaselogin/MapActivity.java
@@ -565,7 +565,7 @@ public class MapActivity extends AppCompatActivity
 
         new NRPlaces.Builder()
                 .listener(MapActivity.this)
-                .key("AIzaSyDXWPIyn84mPX_63PD2oQc6PjMu3cDTn7E")
+                .key("AIzaSyCTqlWqciTWTl6lHhxN2e_-Jx6xK11jlD0")
                 .latlng(location.latitude, location.longitude)//현재 위치
                 .radius(500) //500 미터 내에서 검색
                 .type(PlaceType.RESTAURANT) //음식점
