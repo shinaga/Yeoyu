a5771c5a87d2f20102c0d277b5bde22cabfd81fd
seohabb <seohajb@gmail.com>
2022-09-22 16:15:56 +0900

    test

--- app/src/main/java/com/cookandroid/firebaselogin/MainActivity.java
+++ app/src/main/java/com/cookandroid/firebaselogin/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     static EditText editId,editPasswd;
     Button btnLogin;
     TextView textRegister,textFindPw;
-    int tmp;//Git Test용!!!!!!!!!
+    int tmp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
