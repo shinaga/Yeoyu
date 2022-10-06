886416a4dba858f6897ef55ed74755afe8d903b2
user <shinage2@naver.com>
2022-10-06 15:42:11 +0900

    https://hwanine.github.io/android/Google-map5/

--- app/src/main/java/com/cookandroid/firebaselogin/NoticeviewActivity.java
+++ app/src/main/java/com/cookandroid/firebaselogin/NoticeviewActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class NoticeviewActivity extends AppCompatActivity {
-
+    int number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticeview);
 
+        numberSet();
+
+    }
+
+    private void numberSet() {
 
     }
 }
