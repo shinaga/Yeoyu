d064890c093103f1c223f814592da81a1526e85a
user <shinage2@naver.com>
2022-10-06 17:10:23 +0900

    Intent 구현

--- app/src/main/java/com/cookandroid/firebaselogin/NoticeviewActivity.java
+++ app/src/main/java/com/cookandroid/firebaselogin/NoticeviewActivity.java
@@ -2,7 +2,9 @@ package com.cookandroid.firebaselogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class NoticeviewActivity extends AppCompatActivity {
     int number;
@@ -11,11 +13,13 @@ public class NoticeviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticeview);
 
-        numberSet();
+        numberSet();//받아온 게시글의 number를 가져옴
 
     }
 
     private void numberSet() {
-
+        Intent intent = getIntent();
+        number = Integer.parseInt(intent.getExtras().getString("number"));
+        Toast.makeText(this,number+"",Toast.LENGTH_SHORT).show();
     }
 }
