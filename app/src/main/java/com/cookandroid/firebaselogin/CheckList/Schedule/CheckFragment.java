60d137321639e0b4879a51bb11f90718d499ac89
shinaga <shinage2@naver.com>
2022-12-08 12:56:39 +0900

    스케줄 하단바 임시 적용

--- app/src/main/java/com/cookandroid/firebaselogin/CheckList/Schedule/CheckFragment.java
+++ app/src/main/java/com/cookandroid/firebaselogin/CheckList/Schedule/CheckFragment.java
@@ -1,32 +1,41 @@
 package com.cookandroid.firebaselogin.CheckList.Schedule;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.cookandroid.firebaselogin.AddNewTask;
 import com.cookandroid.firebaselogin.CheckListActivity;
 import com.cookandroid.firebaselogin.DatabaseHandler;
+import com.cookandroid.firebaselogin.DialogCloseListener;
 import com.cookandroid.firebaselogin.HomeActivity;
 import com.cookandroid.firebaselogin.R;
 import com.cookandroid.firebaselogin.RecyclerItemTouchHelper;
+import com.cookandroid.firebaselogin.ScheduleEdit;
 import com.cookandroid.firebaselogin.ToDoAdapter;
 import com.cookandroid.firebaselogin.ToDoModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class CheckFragment extends Fragment {
+public class CheckFragment extends Fragment implements DialogCloseListener, View.OnClickListener{
     private DatabaseHandler db;
 
     private RecyclerView tasksRecyclerView;
@@ -40,8 +49,6 @@ public class CheckFragment extends Fragment {
 
     public static final int REQUEST_ADD = 1;
     public static final int REQUEST_EDIT = 2;
-
-    private Context context;
 
     private Button addBtn;
     private Button clearBtn;
@@ -86,7 +93,110 @@ public class CheckFragment extends Fragment {
             }
         });
 
-        //init();
+        init();
         return view;
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
+    }
+
+    private void init() {
+
+        addBtn = view.findViewById(R.id.add_btn);
+        clearBtn = view.findViewById(R.id.clear_btn);
+        saveBtn = view.findViewById(R.id.save_btn);
+        loadBtn = view.findViewById(R.id.load_btn);
+
+
+        timetable = view.findViewById(R.id.timetable);
+        timetable.setHeaderHighlight(2);
+        initView();
+    }
+
+    private void initView() {
+        addBtn.setOnClickListener(this);
+        clearBtn.setOnClickListener(this);
+        saveBtn.setOnClickListener(this);
+        loadBtn.setOnClickListener(this);
+
+
+        timetable.setOnStickerSelectEventListener(new TimeTableView.OnStickerSelectedListener() {
+            @Override
+            public void OnStickerSelected(int idx, ArrayList<Schedule> schedules) {
+                Intent i = new Intent(con, ScheduleEdit.class);
+                i.putExtra("mode", REQUEST_EDIT);
+                i.putExtra("idx", idx);
+                i.putExtra("schedules", schedules);
+                startActivityForResult(i, REQUEST_EDIT);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_btn:
+                Intent i = new Intent(con, ScheduleEdit.class);
+                i.putExtra("mode", REQUEST_ADD);
+                startActivityForResult(i, REQUEST_ADD);
+                break;
+
+            case R.id.clear_btn:
+                timetable.removeAll();
+                break;
+
+            case R.id.save_btn:
+                saveByPreference(timetable.createSaveData());
+                break;
+
+            case R.id.load_btn:
+                loadSavedData();
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ADD:
+                if (resultCode == ScheduleEdit.RESULT_OK_ADD) {
+                    ArrayList<Schedule> item = (ArrayList<Schedule>) data.getSerializableExtra("schedules");
+                    timetable.add(item);
+                }
+                break;
+            case REQUEST_EDIT:
+                if (resultCode == ScheduleEdit.RESULT_OK_EDIT) {
+                    int idx = data.getIntExtra("idx", -1);
+                    ArrayList<Schedule> item = (ArrayList<Schedule>) data.getSerializableExtra("schedules");
+                    timetable.edit(idx, item);
+                } else if (resultCode == ScheduleEdit.RESULT_OK_DELETE) {
+                    int idx = data.getIntExtra("idx", -1);
+                    timetable.remove(idx);
+                }
+                break;
+        }
+    }
+
+    private void saveByPreference(String data){
+        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(con);
+        SharedPreferences.Editor editor = mPref.edit();
+        editor.putString("timetable_demo",data);
+        editor.commit();
+        Toast.makeText(con,"saved!",Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadSavedData(){
+        timetable.removeAll();
+        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(con);
+        String savedData = mPref.getString("timetable_demo","");
+        if(savedData == null && savedData.equals("")) return;
+        timetable.load(savedData);
+        Toast.makeText(con,"loaded!",Toast.LENGTH_SHORT).show();
     }
 }
